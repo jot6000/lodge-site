bb80aa2ae51ea28d99b0fd69571f9d9f3538b5f6
Joe Mercer <joemercer50@googlemail.com>
2019-05-27 16:18:47 +0100

    Set up routing and state for app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Home() {
+  return(
+      <>
+        <h2>Welcome to Biwagani, a board gaming league hosted by Joe Mercer @TheLodge</h2>
+        <div>Next event card here</div>
+        <Link to="/notHome">Click here to not go home</Link>
+      </>
+    )
+}
+
+function NotHome() {
+  return(
+      <>
+        <h2>Not the home page</h2>
+      </>
+    )
+}
+
+interface IState {
+  logedIn: boolean;
+  loginPopupOpen: boolean;
+  isAdmin: boolean;
+}
+
+interface IProps {}
+
+class App extends React.Component<IProps, IState> {
+  constructor(props: IProps) {
+    super(props);
+
+    this.state = {
+      logedIn: true,
+      loginPopupOpen: false,
+      isAdmin: true
+    };
+}
+  render() {
+    if(this.state.logedIn === true){
+      return <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">Biwagani</Link>
+            {this.state.isAdmin === true &&
+              <div>Admin Portal</div>
+            }
+            <div>Profile Page</div>
+          </header>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/notHome" exact component={NotHome} />
+        </div>
+      </Router>
+    }
+    else{
+      return <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">Biwagani</Link>
+            <div>Login / Register</div>
+          </header>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/notHome" exact component={NotHome} />
+        </div>
+      </Router>
+    }
+  }
 }
 
 export default App;
